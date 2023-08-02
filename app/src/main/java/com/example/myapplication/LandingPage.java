802e521fc8449267afbf03d1cc8d26078c2a96c8
githukelvin <kelvingithu019@gmail.com>
2023-08-02 12:12:58 +0300

    updated the landing_page xml

---
+++ app/src/main/java/com/example/myapplication/LandingPage.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.landing_page);
+
+        // Your code for the LandingPage activity goes here
+        // You can initialize UI elements, handle user interactions, etc.
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+//import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.click_go);
+        button.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LandingPage.class)));
     }
 }
